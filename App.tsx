c19535e72cd3a5e3c11a6f600ffd5e3a04800a05
Prerit Bhandari <patrioticprerit@gmail.com>
2020-11-21 19:24:10 +0545

    [Added] amplify in project

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
+import useCachedResources from "./hooks/useCachedResources";
+import useColorScheme from "./hooks/useColorScheme";
+import Navigation from "./navigation";
+
+//aws settings
+import Amplify from "aws-amplify";
+import config from "./aws-exports";
+Amplify.configure(config);
 
 export default function App() {
   const isLoadingComplete = useCachedResources();
