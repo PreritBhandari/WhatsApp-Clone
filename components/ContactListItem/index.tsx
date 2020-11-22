48f67398e9a7321ba422c75609e3bd2ad01fbebf
Prerit Bhandari <patrioticprerit@gmail.com>
2020-11-22 20:29:39 +0545

    [Displayed] name , dp and linked authenticated user with chat users

--- components/ContactListItem/index.tsx
+++ components/ContactListItem/index.tsx
@@ -3,6 +3,8 @@ import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 import { User } from "../../types";
 import styles from "./style";
 import { useNavigation } from "@react-navigation/native";
+import { API, graphqlOperation, Auth } from "aws-amplify";
+import { createChatRoom, createChatRoomUser } from "../../graphql/mutations";
 
 export type ContactListItemProps = {
   user: User;
@@ -13,13 +15,51 @@ const ContactListItem = (props: ContactListItemProps) => {
 
   const navigation = useNavigation();
 
-  const onClick = () => {
-    ////navigate to chat
-    // navigation.navigate("ChatRoom", {
-    //   id: chatRoom.id,
-    //   name: user.name,
-    //   image: user.imageUri,
-    // });
+  const onClick = async () => {
+    try {
+      const newChatRoomData = await API.graphql(
+        graphqlOperation(createChatRoom, { input: {} })
+      );
+
+      if (!newChatRoomData.data) {
+        console.log("Failed to create Chat Room");
+        return;
+      }
+
+      const newChatRoom = newChatRoomData.data.createChatRoom;
+
+      // adding user to chat room
+
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: user.id,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      // add us i.e authenticated user /our user
+      const userInfo = await Auth.currentAuthenticatedUser();
+      await API.graphql(
+        graphqlOperation(createChatRoomUser, {
+          input: {
+            userID: userInfo.attributes.sub,
+            chatRoomID: newChatRoom.id,
+          },
+        })
+      );
+
+      //navigate to chat
+
+      navigation.navigate("ChatRoom", {
+        id: newChatRoom.id,
+        name: user.name,
+        image: user.imageUri,
+      });
+    } catch (e) {
+      console.log(e);
+    }
   };
   return (
     <TouchableWithoutFeedback onPress={onClick}>
