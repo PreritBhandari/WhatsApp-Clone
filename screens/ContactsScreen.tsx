2428aecd1c4ccf9ca45cf515835e7a5477377a8c
Prerit Bhandari <patrioticprerit@gmail.com>
2020-11-22 19:05:14 +0545

    [Users] displayed all im ContactList

--- screens/ContactsScreen.tsx
+++ screens/ContactsScreen.tsx
@@ -3,11 +3,27 @@ import { StyleSheet } from "react-native";
 
 import { Text, View } from "../components/Themed";
 
-import users from "../data/users";
+// import users from "../data/users";
 import { FlatList } from "react-native-gesture-handler";
 import NewMessageButton from "../components/NewMessageButton";
 import ContactListItem from "../components/ContactListItem";
+import { API, graphqlOperation } from "aws-amplify";
+import { listUsers } from "../graphql/queries";
 export default function ContactsScreen() {
+  const [users, setUsers] = React.useState([]);
+  React.useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const usersData = await API.graphql(graphqlOperation(listUsers));
+
+        setUsers(usersData.data.listUsers.items);
+      } catch (e) {
+        console.log(e);
+      }
+    };
+    fetchUsers();
+  }, []);
+
   return (
     <View style={styles.container}>
       <FlatList
